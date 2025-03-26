385481b59f6713cd562a7cacacf367894cde4c26
Yassin97440 <yassinabdulla614@gmail.com>
2025-03-26 22:44:54 +0100

    add var env for apikey

--- RAG_TS/RAGUtils.ts
+++ RAG_TS/RAGUtils.ts
@@ -1,5 +1,5 @@
 import { ChatMistralAI } from "@langchain/mistralai";
-
+import { MISTRAL_API_KEY } from "./varEnv";
 
 export class RAGUtils {
 
@@ -7,8 +7,8 @@ export class RAGUtils {
     public getLlm() {
       return new ChatMistralAI({
           model: "mistral-large-latest",
-          temperature: 0
-          
+          temperature: 0,
+          apiKey: MISTRAL_API_KEY
         });
     }
 }
