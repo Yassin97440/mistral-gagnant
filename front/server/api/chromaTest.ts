cadba2a7fc6f609a21759b7fc5d494a4fb97c54c
Yassin97440 <yassinabdulla614@gmail.com>
2025-04-07 01:12:27 +0200

    delete useless files

--- front/server/api/chromaTest.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentHandler } from "~~/services/RAG/DocumentHandler"
-
-const documentHandler = new DocumentHandler()
-export default defineEventHandler(async (event) => {
-    console.log("ca va commencer! incroyable")
-    documentHandler.processAllDocumentsWithPagination()
-    console.log("storing terminé! incroyable")
-
-})
