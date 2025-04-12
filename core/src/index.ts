0e41067901d65a58b58ae651806e61ec1c47a8df
Yassin97440 <yassinabdulla614@gmail.com>
2025-04-13 01:05:09 +0200

    add modules to index ts

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,9 +1,9 @@
 export * from './data';
 export * from './processing';
-// export * from './tools';
+export * from './tools';
 // export * from './agents';
 // export * from './workflow';
-// export * from './utils';
+export * from './utils';
 
 export const maFonction = () => {
   return "Hello from core module!";
